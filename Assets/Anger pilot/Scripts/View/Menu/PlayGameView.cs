f08332bb77e0e2556382c71890a4a5f6a6c81fd5
VladRadov <picvladislav@yandex.ru>
2024-05-06 23:52:42 +0300

    feat: add view play game

---
+++ Assets/Anger pilot/Scripts/View/Menu/PlayGameView.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayGameView : MonoBehaviour
+{
+    [SerializeField] private Button _startGame;
+
+    private void Start()
+    {
+        _startGame.onClick.AddListener(() => { ManagerScenes.Instance.LoadAsyncFromCoroutine("Game"); });
+    }
+
+    private void OnValidate()
+    {
+        if (_startGame == null)
+            _startGame = GetComponent<Button>();
+    }
+}
